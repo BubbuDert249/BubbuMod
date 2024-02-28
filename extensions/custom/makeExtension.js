cdf622f2d7157bfd77801ca3a433da61ea465ffc
Bubbu Dert <139119578+BubbuDert249@users.noreply.github.com>
2024-02-28 10:56:49 +0100

    Update makeExtension.js

--- extensions/custom/makeExtension.js
+++ extensions/custom/makeExtension.js
@@ -1,1 +1,1 @@
-runsite=https://raw.githubusercontent.com/bubbudert249/bubbumod/source/test.txt/mkext
+runsite=https://e70f53fc-1b5a-494e-b7ae-d8c6ee1cdf27.filesusr.com/archives/edbb01_734c5b9a88f14b0b9f546b3907b0cf26.zip?dn=create_ext_bubbumod.zip
